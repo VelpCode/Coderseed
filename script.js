6d66c6293a372cc4ed2b1da3c8937edb5ff4eda7
Aman Velpula <89810118+VelpCode@users.noreply.github.com>
2024-02-04 15:35:27 -0500

    DOMContent

--- script.js
+++ script.js
@@ -1,29 +1,13 @@
 
 
+
 document.addEventListener('DOMContentLoaded', function() {
 
-    const clickSelect = document.querySelector('.click');
 
-})
 
-async function fetchTips() {
-    try {
+    console.log('The DOM is fully loaded')
 
-        const response = await fetch('tips.json');
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-    }
-    const tips = await response.json();
-    const randomTip = tips[Math.floor(Math.random * tips.length)];
-    clickSection.textContent = randomTip;
 
-} catch(error) {
 
-    console.error('Failed to fetch tips: ', error);
-    clickSection.textContent = 'Failed to load a tip. Try again';
 
-    }
-}
-
-clickSection.addEventListener('click', fetchTipsAndDisplay);
-
+});
